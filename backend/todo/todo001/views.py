418d843e718c0725440d305e5ddc5cb6b98a96bc
RACHA LAXMI RAVI SHANKAR <ravishankarracha@gmail.com>
2025-04-18 16:09:04 +0530

    using django rest framework

--- backend/todo/todo001/views.py
+++ backend/todo/todo001/views.py
@@ -1,24 +1,14 @@
 from django.shortcuts import render,redirect
+from rest_framework import generics
 from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
-def todo_list(request):
-    if request.method == 'POST':
-        #getting the data from the client
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        completed = request.POST.get('completed') == 'on'
-        due_date = request.POST.get('due_date')
+class todo_list(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
-        # Save the new todo to the database
-        todo = Todo(
-            title=title,
-            description=description,
-            completed=completed,
-            due_date=due_date
-        )
-        todo.save() # Save the new todo to the database and compulsary to write this line
-        return redirect('todo_list')   # Redirect to avoid form resubmission
-    todos = Todo.objects.all().order_by('-due_date')
-    return render(request, 'todo_list.html',{'todos': todos})
 
+class todo_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
